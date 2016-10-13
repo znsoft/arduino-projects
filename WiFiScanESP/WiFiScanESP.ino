677742de0487756991493589ad0d04f7b12c9d1f
Eugenie <zn@bk.ru>
2016-10-13 15:18:36 +1000

    Простенький сканер вифи сетей

---
+++ WiFiScanESP/WiFiScanESP.ino
@@ -0,0 +1,167 @@
+/*
+    This sketch demonstrates how to scan WiFi networks.
+    The API is almost the same as with the WiFi Shield library,
+    the most obvious difference being the different file you need to include:
+*/
+#include "ESP8266WiFi.h"
+//#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+
+/* Set these to your desired credentials. */
+const char *ssid = "PPAP";
+const char *password = "12345678";
+ESP8266WebServer server ( 80 );
+IPAddress myIP;
+
+const int led = 13;
+
+void handleRoot() {
+  digitalWrite ( led, 1 );
+  char temp[400];
+  int sec = millis() / 1000;
+  int min = sec / 60;
+  int hr = min / 60;
+
+  snprintf ( temp, 400,
+
+             "<html>\
+  <head>\
+    <meta http-equiv='refresh' content='5'/>\
+    <title>ESP8266 Demo</title>\
+    <style>\
+      body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\
+    </style>\
+  </head>\
+  <body>\
+    <h1>Hello from ESP8266!</h1>\
+    <p>Uptime: %02d:%02d:%02d</p>\
+    <br><a href=\"/scan\">Scan WIFI</a><br>\
+    <img src=\"/test.svg\" />\
+  </body>\
+</html>",
+
+             hr, min % 60, sec % 60
+           );
+  server.send ( 200, "text/html", temp );
+  digitalWrite ( led, 0 );
+}
+
+
+void handleNotFound() {
+  digitalWrite ( led, 1 );
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += ( server.method() == HTTP_GET ) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n<br><a href=/test.svg>TEST GRAPH</a><br><a href=/inline>INline</a>";
+
+  for ( uint8_t i = 0; i < server.args(); i++ ) {
+    message += " " + server.argName ( i ) + ": " + server.arg ( i ) + "\n";
+  }
+
+  server.send ( 404, "text/plain", message );
+  digitalWrite ( led, 0 );
+}
+
+
+void setup() {
+  Serial.begin(115200);
+  pinMode ( led, OUTPUT );
+  digitalWrite ( led, 0 );
+  delay(1000);
+  Serial.print("Configuring access point...");
+  WiFi.mode(WIFI_AP_STA);
+  /* You can remove the password parameter if you want the AP to be open. */
+  WiFi.softAP(ssid);//password);http://esp8266.github.io/Arduino/versions/2.1.0-rc1/doc/libraries.html
+  // Set WiFi to station mode and disconnect from an AP if it was previously connected
+  //  WiFi.disconnect();
+
+  myIP = WiFi.softAPIP();
+  Serial.println(myIP);
+  server.on ( "/", handleRoot );
+  server.on ( "/test.svg", drawGraph );
+  server.on ( "/scan", Scan );
+  server.on ( "/inline", []() {
+    server.send ( 200, "text/plain", "this works as well" );
+  } );
+  server.onNotFound ( handleNotFound );
+  server.begin();
+  Serial.println ( "HTTP server started" );
+
+  Serial.println("Setup done");
+}
+
+
+void drawGraph() {
+  String out = "";
+  char temp[100];
+  out += "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"400\" height=\"150\">\n";
+  out += "<rect width=\"400\" height=\"150\" fill=\"rgb(250, 230, 210)\" stroke-width=\"1\" stroke=\"rgb(0, 0, 0)\" />\n";
+  out += "<g stroke=\"black\">\n";
+  int y = rand() % 130;
+  for (int x = 10; x < 390; x += 10) {
+    int y2 = rand() % 130;
+    sprintf(temp, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke-width=\"1\" />\n", x, 140 - y, x + 10, 140 - y2);
+    out += temp;
+    y = y2;
+  }
+  out += "</g>\n</svg>\n";
+
+  server.send ( 200, "image/svg+xml", out);
+}
+
+void Scan() {
+
+  digitalWrite ( led, 1 );
+  String scanTemp;
+
+  //char temp[400], scanTemp[500];
+  int sec = millis() / 1000;
+  int n = WiFi.scanNetworks();
+  sec = (millis() / 1000) - sec;
+
+
+  scanTemp = "<html>\
+  <head>\
+    <meta http-equiv='refresh' content='5'/>\
+    <title>ESP8266 Demo</title>\
+    <style>\
+      body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\
+    </style>\
+  </head>\
+  <body>\
+    <h1>Scan</h1>";
+
+
+  if (n == 0)
+    scanTemp +=  "<br>-no networks found";
+  else
+  {
+    scanTemp += "<br>----networks---- " + String( n , DEC );
+    for (int i = 0; i < n; i++) {
+      scanTemp +=  "<br>" + String( i , DEC ) + ". ";
+      scanTemp += WiFi.SSID(i);
+      scanTemp += " (";
+      scanTemp +=  WiFi.RSSI(i);
+      scanTemp +=  ")";
+      scanTemp += (WiFi.encryptionType(i) == ENC_TYPE_NONE) ? " " : "*";
+
+    }
+
+  }
+  scanTemp += "<br>end</body>\
+</html>";
+  server.send ( 200, "text/html", scanTemp);
+
+  digitalWrite ( led, 0 );
+
+
+}
+
+void loop() {
+  server.handleClient();
+}
